0704d43d06b6f9a98eba5568e01229e134daa75c
Toshiyuki Kasai <tocguitar1@gmail.com>
2020-05-27 00:57:37 +0900

    Delete variables.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,5 +9,5 @@ provider "aws" {
 
 module "network" {
   source   = "./modules/aws/network"
-  vpc_cidr = var.vpc_cidr
+  vpc_cidr = "10.0.0.0/16"
 }
--- terraform/variables.tf
+++
@@ -1,3 +0,0 @@
-variable "vpc_cidr" {
-  default = "10.0.0.0/16"
-}
